f7a94aee85d8ac94e3ec103f52ee2a024508c1c5
Ben Thomas <bthomas2622@gmail.com>
2015-11-05 19:48:10 -0500

    changed art assets and added winning text

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,14 +1,14 @@
-// Enemies our player must avoid
+// Enemy object that our player must avoid
 var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = "images/enemy-bug.png";
     this.x = x;
     this.y = y;
+    // speed of enemy adjustment
     this.speed = 200;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = "images/shark.png";
+
 };
 
 // Update the enemy's position, required method for game
@@ -19,12 +19,15 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x = (dt*this.speed) + this.x;
 
+    //if statement that checks to see if the enemy has run into a player any time its position changes (dt)
     if (player.x < this.x + 100 & player.x > this.x - 50){
         if (player.y > this.y - 50 & player.y < this.y + 50){
             player.reset();
         };
     };
 
+    //if statement that returns the enemy to the start of the track when it goes off screen
+    //Math.random() returns a "random" value between 0 and 1 that I use to stagger start of enemy
     if (this.x > 600) {
         this.x = (-400)*Math.random();
     };
@@ -35,78 +38,81 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+//player object function. Used "PlayerObj" for easy distinction from "player" instance of "PlayerObj"
 var PlayerObj = function(){
-    this.sprite = "images/enemy-bug.png";
+    //sprite that represents player
+    this.sprite = "images/penguin.png";
+    //default starting position for player
     this.x = 200;
     this.y = 385;
 
 };
 
+//created a function that returns x position of player so i could print to console for bug checking
+//realized later could be done in much easier ways 
 PlayerObj.prototype.xgetter = function() {
     return this.x;
 };
 
-//player methods run off player prototype 
+//I chose to update the players position in the handleinput function
 PlayerObj.prototype.update = function() {
 };
 
 //method to update player position with arrow keys
 PlayerObj.prototype.handleInput = function(input){
+    //switch statement to determine key pressed is more efficient than many if statements
     switch(input) {
         case "down":
+            //only lets player go down if not at bottom of screen
             if (this.y < 385){
                 this.y = this.y + 80;
             }
             break;
         case "up": 
             this.y = this.y - 80;
+            //if statement resents the player once they reach the end of road
+            //clear the text from the winning run
+            ctx.clearRect(0, 0, 200, 200);
             if (this.y <= -20){
                 this.reset();
+                //tell player they saved the penguin!
+                ctx.font = "20pt Arial";
+                ctx.fillStyle = "red";
+                ctx.fillText("Nice Work!", 0, 30);
             }
             break;
         case "right":
+            //moves player to right if they are not at edge of grid
             if (this.x < 380){
                 this.x = this.x + 90;
             }
             break;
         case "left":
+            //moves player to left if they are not at edge of grid
             if (this.x > 100){
                 this.x = this.x - 90;
             }
             break;
     }
-
-    // for (enemy in allEnemies){
-    //     console.log(allEnemies[enemy].x);
-    //     if (this.x < allEnemies[enemy].x + 50 & this.x > allEnemies[enemy].x - 50){
-    //         if (this.y > allEnemies[enemy].y - 50 * this.y < allEnemies[enemy].y + 50){
-    //             this.reset();
-    //         };
-    //     };
-    // };
-
+    //printing coordinates of player helps me adjust code from playing the game feedback
     console.log("x" + this.x);
     console.log("y" + this.y);
 };
 
+//draws the player on the screen
 PlayerObj.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
+//function to "restart" game when player hits an enemy or finishes 
 PlayerObj.prototype.reset = function() {
     this.x = 200;
     this.y = 385;
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// Instantiation of player and enemy objects
+// enemy objects are within an array since multiple  
 var player = new PlayerObj();
 var allEnemies = [new Enemy(-500*Math.random(), 225), new Enemy(-500*Math.random(), 150), new Enemy(-500*Math.random(), 50)];
 
--- frontend-nanodegree-arcade-game-master/js/engine.js
+++ frontend-nanodegree-arcade-game-master/js/engine.js
@@ -108,12 +108,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/snow.png',   // Top row is water
+                'images/waves.png',   // Row 1 of 3 of stone
+                'images/waves.png',   // Row 2 of 3 of stone
+                'images/waves.png',   // Row 3 of 3 of stone
+                'images/sand.png',   // Row 1 of 2 of grass
+                'images/sand.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
@@ -167,11 +167,11 @@ var Engine = (function(global) {
      * all of these images are properly loaded our game will start.
      */
     Resources.load([
-        'images/stone-block.png',
-        'images/water-block.png',
-        'images/grass-block.png',
-        'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/penguin.png',
+        'images/shark.png',
+        'images/waves.png',
+        'images/snow.png',
+        'images/sand.png'
     ]);
     Resources.onReady(init);
 
